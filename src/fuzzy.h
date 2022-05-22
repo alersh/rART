2731dd833fc6bac4c24dbcfe2e81095b08fd0759
Albert Ler <albertler@Bueller.local>
2022-05-22 15:33:30 -0400

    Fuzzy model implementation

--- src/fuzzy.h
+++ src/fuzzy.h
@@ -6,17 +6,26 @@
  ****************************************************************************/
 
 #include <Rcpp.h>
+#include "IModel.h"
 using namespace Rcpp;
+
+#ifndef FUZZY_H
+#define FUZZY_H
 
 bool isFuzzy ( List net );
 
-namespace Fuzzy {
+struct Fuzzy : IModel {
+  
+  Fuzzy( List net );
+  int getWeightDimension( int featureDimension );
+  NumericVector newWeight( NumericVector x );
   double activation( List module, NumericVector x, NumericVector w );
-  double TopoPredictActivation ( List module, NumericVector x, NumericVector w );
-  double match( List module, NumericVector x, NumericVector w );
-  NumericVector weightUpdate( List module, NumericVector x, NumericVector w );
-  void trainART( List net, NumericMatrix x );
-  void trainARTMAP( List net, NumericMatrix x, Nullable< NumericVector > vTarget = R_NilValue, Nullable< NumericMatrix > mTarget = R_NilValue );
-  List predictART( List net, int id, NumericMatrix x );
-  List predictARTMAP( List net, NumericMatrix x, Nullable< NumericVector > vTarget, Nullable< NumericMatrix > mTarget = R_NilValue, bool test = false );
-}
+  double TopoPredictActivation (List module, NumericVector x, NumericVector w );
+  double match(List module, NumericVector x, NumericVector w );
+  NumericVector weightUpdate( List module, double learningRate, NumericVector x, NumericVector w );
+  NumericVector processCode( NumericVector x );
+  NumericVector unProcessCode( NumericVector x );
+  NumericMatrix normalizeCode( NumericMatrix x );
+};
+
+#endif
