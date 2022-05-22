df0a9a27b6dca9b0e4883ec1a4d969acaa2912cd
Albert Ler <albertler@Bueller.local>
2022-05-22 15:38:36 -0400

    Added accessories; updated functions to accommodate IModel objects.

--- src/ART.h
+++ src/ART.h
@@ -7,46 +7,71 @@
 
 
 #include <Rcpp.h>
+#include "IModel.h"
 using namespace Rcpp;
 using namespace std;
+
+#ifndef ART_H
+#define ART_H
 
 
 List newART ( int dimension, int num = 1, double vigilance = 0.75, double learningRate = 1.0, int categorySize = 200, int maxEpochs = 20 );
 bool isART ( List net );
-namespace ART {
-        List module ( int id, int dimension, double vigilance = 0.75, double learningRate = 1.0, int categorySize = 100 );
-        NumericVector activation( List module, NumericVector x, std::function< double( List, NumericVector, NumericVector ) > fun );
-        double match( List module, NumericVector x, int weightIndex, std::function< double( List, NumericVector, NumericVector ) > fun );
-        int weightUpdate( List module, NumericVector x, int weightIndex, std::function< NumericVector( List, NumericVector, NumericVector ) > fun );
-        void newCategory( List net, NumericVector x );
-        int learn( List net,
-                   int id,
-                   NumericVector d,
-                   std::function< double( List, NumericVector, NumericVector ) > activationFun,
-                   std::function< double( List, NumericVector, NumericVector ) > matchFun,
-                   std::function< NumericVector( List, NumericVector, NumericVector ) > weightUpdateFun );
-        int classify( List net,
-                      int id,
-                      NumericVector d,
-                      std::function< double( List, NumericVector, NumericVector ) > activationFun,
-                      std::function< double( List, NumericVector, NumericVector ) > matchFun );
 
-        void train( List net,
-                    NumericMatrix x,
-                    std::function< NumericVector( NumericVector ) > codeFun,
-                    std::function< double( List, NumericVector, NumericVector ) > activationFun,
-                    std::function< double( List, NumericVector, NumericVector ) > matchFun,
-                    std::function< NumericVector( List, NumericVector, NumericVector ) > weightUpdateFun );
-        List predict( List net,
-                      int id,
-                      NumericMatrix x,
-                      std::function< NumericVector( NumericVector ) > codeFun,
-                      std::function< double( List, NumericVector, NumericVector ) > activationFun,
-                      std::function< double( List, NumericVector, NumericVector ) > matchFun );
+namespace ART {
+        List create ( int dimension, int num = 1, double vigilance = 0.75, double learningRate = 1.0, int categorySize = 100, int maxEpochs = 20 );
+        int getID( List module );
+        int getJmax( List module, int matchIndex = 0 );
+        void setJmax( List module, int J, int matchIndex = 0 );
+        void addJmax( List module, int J );
+        int getMaxEpochs( List net );
+        void setEpoch( List net, int epoch );
+        void incChange( List module, int index );
+        int getChangeSum( List module );
+        IntegerVector getChangeVector( List module );
+        void setChangeVector( List module, IntegerVector v );
+        void changeReset( List module );
+        bool unchanged( List module );
+        int getNumModules( List net );
+        List module ( int id, double vigilance = 0.75, double learningRate = 1.0, int categorySize = 100 );
+        List getModule( List net, int moduleID );
+        void setModule( List net, List module );
+        bool hasMoreModules( List net, int currentModuleID );
+        NumericMatrix getWeightMatrix( List module );
+        void setWeightMatrix( List module, NumericMatrix w );
+        void initWeights( List module, int dim );
+        NumericVector getWeight( List module, int weightIndex );
+        void setWeight( List module, int weightIndex, NumericVector w );
+        double getRho( List module );
+        void setRho( List module, double rho );
+        int getNumCategories( List module );
+        void setNumCategories( List module, int x );
+        double getLearningRate( List module );
+        void setLearningRate( List module, double learningRate );
+        int getDimension( List module );
+        int getCapacity( List module );
+        int getCounter( List module, int weightIndex );
+        IntegerVector getCounterVector( List module );
+        double getAlpha( List module );
+        double getEpsilon( List module );
+        void setCounterVector( List module, IntegerVector v );
+        void counterReset( List module );
+        void initModule( List module, int weightDimension );
+        void init( IModel &model );
+        
+        NumericVector activation( IModel &model, List module, NumericVector x );
+        double match( IModel &model, List module, int weightIndex , NumericVector x);
+        void weightUpdate( IModel &model, List module, int weightIndex, NumericVector x );
+        void counterUpdate( List module, int nodeIndex );
+        void newCategory( IModel &model, List module, NumericVector x );
+        void learn( IModel &model, int id, NumericVector d );
+        int classify( IModel &model, int id, NumericVector d );
+        
+        void train( IModel &model, NumericMatrix x );
+        List predict( IModel &model, int id, NumericMatrix x );
 }
 
-void trainART ( List net, NumericMatrix x );
+void train ( List net, NumericMatrix x );
+List predict ( List net, int id, NumericMatrix x );
 
-List predictART ( List net, NumericMatrix x );
-
-
+#endif
