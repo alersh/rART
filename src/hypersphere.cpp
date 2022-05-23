c6512e6d43909840b09baac86d8d94527dadf6d9
Albert Ler <albertler@Bueller.ht.home>
2022-05-23 10:08:22 -0400

    Created hypersphere model

--- src/hypersphere.cpp
+++ src/hypersphere.cpp
@@ -15,10 +15,9 @@
 
 #include <Rcpp.h>
 #include "ART.h"
-#include "ARTMAP.h"
-#include "TopoART.h"
+//#include "ARTMAP.h"
 #include "utils.h"
-#include "fuzzy.h"
+#include "hypersphere.h"
 using namespace Rcpp;
 using namespace std;
 
@@ -27,153 +26,104 @@ bool isHypersphere ( List net ){
   return as<string>( net.attr( "rule" ) ).compare( "hypersphere" ) == 0;
 }
 
-namespace Hypersphere {
+Hypersphere::Hypersphere( List net ) : IModel( net ){}
 
-  double norm( NumericVector x, NumericVector m ){
-    return sqrt( sum( na_omit( pow( x - m, 2 ) ) ) );
-  }
-
-  double R_bar( NumericMatrix x ){
-    NumericVector maximum = colMax( x );
-    NumericVector minimum = colMin( x );
-
-    double d = sqrt( 0.5 * sum( na_omit( pow( maximum - minimum, 2 ) ) ) );
-    return d;
-  }
-
-  void initWeight( List net, NumericMatrix x ){
-    int numModules = net["numModules"];
-    double rbar = R_bar( x );
-    for ( int i = 0; i < numModules; i++ ){
-      List module = as<List>( net["module"] )[i];
-      module.push_back( rbar, "R_bar" );
-      NumericMatrix w = no_init( module["capacity"], as<int>( module["dimension"] ) + 1 );
-      module["w"] = w;
-      as<List>( net["module"] )( i ) = module;
-    }
-
-    if ( isARTMAP( net ) ){
-      List module_ab = as<List>( net["mapfield"] )["ab"];
-      module_ab.push_back( rbar, "R_bar" );
-      as<List>( net["mapfield"] )["ab"] = module_ab;
-    }
-  }
-
-  double activation( List module, NumericVector x, NumericVector w ){
-    int dimension = module["dimension"];
-    double R = w[dimension];
-    NumericVector m = w[Range( 0, dimension - 1 )];
-    double R_bar = module["R_bar"];
-    double maximum = max( na_omit( NumericVector::create( R, norm( x, m ) ) ) );
-    double a = ( R_bar - maximum )/( R_bar - R + as<double>( module["alpha"] ) );
-
-    return a;
-  }
-
-  double TopoPredictActivation ( List module, NumericVector x, NumericVector w ){
-    int dimension = module["dimension"];
-    double R = w[dimension];
-    NumericVector m = w[Range( 0, dimension - 1 )];
-    double R_bar = module["R_bar"];
-    double maximum = max( na_omit( NumericVector::create( norm( x, m ) - R, 0 ) ) );
-    return 1 - maximum/( 2*R_bar );
-  }
-
-  double match( List module, NumericVector x, NumericVector w ){
-    int dimension = module["dimension"];
-    double R = w[dimension];
-    NumericVector m = w[Range( 0, dimension - 1 )];
-    double R_bar = module["R_bar"];
-    double maximum = max( na_omit( NumericVector::create( R, norm( x, m ) ) ) );
-    return 1 - maximum/R_bar;
-  }
-
-  NumericVector weightUpdate( List module, NumericVector x, NumericVector w ){
-
-    int dimension = module["dimension"];
-    double b = module["beta"];
-    double R = w[dimension];
-    NumericVector m = w[Range( 0, dimension - 1 )];
-    double dis = norm( x, m );
-    NumericVector mnew;
-    if ( dis < 0.000001 ){
-      mnew = m;
-    }
-    else{
-      double minimum = min( na_omit( NumericVector::create( R, dis ) ) );
-      mnew = m + b/2 * ( x - m ) * ( 1 - minimum/dis );
-    }
-    double Rnew;
-    double maximum = max( na_omit( NumericVector::create( R, dis ) ) );
-    Rnew = R + b/2 * ( maximum - R );
-    mnew.push_back( Rnew );
-    return mnew;
-  }
-
-  void newCategory( List module, NumericVector x ){
-
-    int n = 0;
-    if ( as<int>( module["numCategories"] ) > 0 ){
-      if (as<int>( module["numCategories"] ) == as<NumericMatrix>( module["w"] ).rows() ){
-        // reached the max capacity, so add more rows
-        module["w"] = appendRows( module["w"], module["capacity"] );
-      }
-      int n = as<int>( module["numCategories"] );
-      module["numCategories"] = n + 1;
-    }
-
-    NumericVector w = x;
-    w.push_back( 0 ); // R
-    as<NumericMatrix>( module["w"] )( n,_ ) = w;
-
-  }
-
-  void trainART( List net, NumericMatrix x ){
-    Hypersphere::initWeight( net, x );
-    if ( isART( net ) ){
-      ART::train( net, x, sameCode, activation, match, weightUpdate );
-    }
-    else if ( isTopoART( net ) ){
-      Topo::train( net, x, sameCode, activation, match, weightUpdate);
-    }
-
-  }
-
-  void trainARTMAP( List net, NumericMatrix x, Nullable< NumericVector > vTarget = R_NilValue, Nullable< NumericMatrix > mTarget = R_NilValue ){
-    if ( !isSimplified( net ) ){
-      stop( "Only the simplified form is available for hypersphere learning." );
-    }
-
-    Hypersphere::initWeight( net, x );
-    if ( isARTMAP( net ) ){
-      ARTMAP::train( net, x, vTarget, mTarget, sameCode, activation, match, weightUpdate, weightUpdate );
-    }
-
-  }
-
-  List predictART( List net, int id, NumericMatrix x ){
-    List results;
-    if ( isART( net ) ){
-      results = ART::predict ( net, id, x, sameCode, activation, match );
-    }
-    else if ( isTopoART( net ) ){
-      results = Topo::predict( net, id, x, sameCode, TopoPredictActivation, match );
-    }
-    return results;
-  }
-
-  List predictARTMAP( List net, NumericMatrix x, Nullable< NumericVector > vTarget, Nullable< NumericMatrix > mTarget = R_NilValue, bool test = false ){
-    List results;
-    if ( isARTMAP( net ) ){
-      if ( !isSimplified( net ) ){
-        // only the simplified form is available
-        net.attr( "simplified" ) = true;
-      }
-      results = ARTMAP::predict ( net, x, vTarget, mTarget, sameCode, sameCode, activation, match, test );
-    }
-
-    return results;
-  }
-
+Hypersphere::Hypersphere( List net, NumericMatrix x ) : IModel( net ){
+  
+  // add R_bar to each module
+  initR_bar( x );
 }
 
+void Hypersphere::initR_bar( NumericMatrix x ){
+  double rbar = R_bar( x );
+  int numModules = ART::getNumModules( this->net );
+  for ( int i = 0; i < numModules; i++ ){
+    List module = ART::getModule( this->net, i );
+    module.push_back( rbar, "R_bar" );
+    ART::setModule( this->net, module );
+  }
+  
+  //if ( isARTMAP( this->net ) ){
+  //  List module_ab = as<List>( this->net["mapfield"] )["ab"];
+  //module_ab.push_back( rbar, "R_bar" );
+  //}
+}
+
+double Hypersphere::norm( NumericVector x, NumericVector m ){
+  return sqrt( sum( na_omit( pow( x - m, 2 ) ) ) );
+}
+
+double Hypersphere::R_bar( NumericMatrix x ){
+  NumericVector maximum = colMax( x );
+  NumericVector minimum = colMin( x );
+  
+  double d = sqrt( 0.5 * sum( na_omit( pow( maximum - minimum, 2 ) ) ) );
+  return d;
+}
+
+int Hypersphere::getWeightDimension( int featureDimension ){
+  return featureDimension + 1;
+}
+
+NumericVector Hypersphere::newWeight( NumericVector x ){
+  x.push_back(0);
+  return x;
+}
+
+double Hypersphere::activation( List module, NumericVector x, NumericVector w ){
+  int dimension = w.length();
+  double R = w[dimension - 1];
+  NumericVector m = w[Range( 0, dimension - 2 )];
+  double R_bar = module["R_bar"];
+  double maximum = max( na_omit( NumericVector::create( R, norm( x, m ) ) ) );
+  double a = ( R_bar - maximum )/( R_bar - R + ART::getAlpha( module ) );
+  
+  return a;
+}
+
+double Hypersphere::TopoPredictActivation ( List module, NumericVector x, NumericVector w ){
+  int dimension = w.length();
+  double R = w[dimension - 1];
+  NumericVector m = w[Range( 0, dimension - 2 )];
+  double R_bar = module["R_bar"];
+  double maximum = max( na_omit( NumericVector::create( norm( x, m ) - R, 0 ) ) );
+  return 1 - maximum/( 2*R_bar );
+}
+
+double Hypersphere::match( List module, NumericVector x, NumericVector w ){
+  int dimension = w.length();
+  double R = w[dimension - 1];
+  NumericVector m = w[Range( 0, dimension - 2 )];
+  double R_bar = module["R_bar"];
+  double maximum = max( na_omit( NumericVector::create( R, norm( x, m ) ) ) );
+  return 1 - maximum/R_bar;
+}
+
+NumericVector Hypersphere::weightUpdate( List module, double learningRate, NumericVector x, NumericVector w ){
+  
+  int dimension = w.length();
+  double R = w[dimension - 1];
+  NumericVector m = w[Range( 0, dimension - 2 )];
+  double dis = norm( x, m );
+  NumericVector mnew;
+  if ( dis < 0.000001 ){
+    mnew = m;
+  }
+  else{
+    double minimum = min( na_omit( NumericVector::create( R, dis ) ) );
+    mnew = m + learningRate/2 * ( x - m ) * ( 1 - minimum/dis );
+  }
+  double Rnew;
+  double maximum = max( na_omit( NumericVector::create( R, dis ) ) );
+  Rnew = R + learningRate/2 * ( maximum - R );
+  mnew.push_back( Rnew );
+  return mnew;
+}
+
+NumericVector Hypersphere::processCode( NumericVector x ) {
+  return x;
+}
+
+NumericVector Hypersphere::unProcessCode( NumericVector x ){
+  return x;
+}
--- src/hypersphere.h
+++ src/hypersphere.h
@@ -3,21 +3,41 @@
  *  hypersphere.cpp
  *  Hypersphere ART and Hypersphere ARTMAP
  *
+ *
+ *
+ *  Reference:
+ *  1. Anagnostopoulos, GC, Georgiopoulos, M. (2000) "Hypersphere ART and ARTMAP
+ *  Unsupervised and Supervised, Incremental Learning", Proceedings of the
+ *  IEEE-INNS-ENNS International Joint Conference on Neural Networks. Neural
+ *  Computing: New Challenges and Perspectives for the New Millennium, 6.
+ *
  ****************************************************************************/
 
 #include <Rcpp.h>
+#include "IModel.h"
 using namespace Rcpp;
+using namespace std;
+
+#ifndef HYPERSPHERE_H
+#define HYPERSPHERE_H
 
 bool isHypersphere ( List net );
 
-namespace Hypersphere {
+struct Hypersphere : IModel{
+  Hypersphere( List net );
+  Hypersphere( List net, NumericMatrix x );
+  void initR_bar( NumericMatrix x );
+  double norm( NumericVector x, NumericVector m );
+  double R_bar( NumericMatrix x );
+  int getWeightDimension( int featureDimension );
+  NumericVector newWeight( NumericVector x );
+  double activation( List module, NumericVector x, NumericVector w );
+  double TopoPredictActivation ( List module, NumericVector x, NumericVector w );
+  double match( List module, NumericVector x, NumericVector w );
+  NumericVector weightUpdate( List module, double learningRate, NumericVector x, NumericVector w );
+  NumericVector processCode( NumericVector x );
+  NumericVector unProcessCode( NumericVector x );
+  
+};
 
-  void trainART( List net, NumericMatrix x );
-
-  void trainARTMAP( List net, NumericMatrix x, Nullable< NumericVector > vTarget = R_NilValue, Nullable< NumericMatrix > mTarget = R_NilValue );
-
-  List predictART( List net, int id, NumericMatrix x );
-
-  List predictARTMAP( List net, NumericMatrix x, Nullable< NumericVector > vTarget, Nullable< NumericMatrix > mTarget = R_NilValue, bool test = false );
-
-}
+#endif
