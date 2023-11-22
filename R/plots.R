81fb1a543585ff380f7624d6541a6bf33bd4af46
Albert Ler <albertler@Bueller.local>
2023-11-22 14:59:28 -0500

    Choose the weight columns to plot

--- R/plots.R
+++ R/plots.R
@@ -4,11 +4,15 @@
 #' @param net An ART network
 #' @param id The numeric id of the module to be plotted. The base module ART a (default) has an id = 0. The ids of the higher order module are incremented.
 #' @param .data The data frame used for plotting the data. It must have 2 columns corresponding to the x and y values, respectively.
+#' @param x The variable to plot along the x axis. If it is not specified, then the first column of the data and the weight matrix
+#' will be used.
+#' @param y The variable to plot along the y axis. If it is not specified, then the second column of the data and the weight matrix
+#' will be used.
 #' @return A ggplot
 #' @importFrom ggplot2 ggplot geom_point aes_
 #' @importFrom rlang sym
 #' @export
-plot.ART <- function(net, id = 0, .data){
+plot.ART <- function(net, id = 0, .data, x = NULL, y = NULL){
   if (is.matrix(.data)){
     .data <- tryCatch(
       as.data.frame(.data),
@@ -23,21 +27,28 @@ plot.ART <- function(net, id = 0, .data){
   module <- getModuleById(net, id)
 
   cols <- colnames(.data)
-
-  ggplot() + geom_point(data = .data, aes_(sym(cols[1]), sym(cols[2]))) +
-    drawWeight(structure(module$w, class = append(class(module$w), getRule(net))))
+  if (is.null(x)) x <- cols[1]
+  if (is.null(y)) y <- cols[2]
+  
+  ggplot() + geom_point(data = .data[, c(x, y)], aes_(sym(x), sym(y))) +
+    drawWeight(structure(module$w, class = append(class(module$w), getRule(net))),
+               x = x, y = y)
 
 }
 #' plot.ARTMAP
 #' @description Plot a 2 dimensional Fuzzy ARTMAP category distribution
 #' @param net An ARTMAP network
 #' @param .data The data frame used for plotting the data. It must have 2 columns specifying x and y values.
+#' @param x The variable to plot along the x axis. If it is not specified, then the first column of the data and the weight matrix
+#' will be used.
+#' @param y The variable to plot along the y axis. If it is not specified, then the second column of the data and the weight matrix
+#' will be used.
 #' @param classes The class labels associated with the data points.
 #' @param dummyCodeMap The dummy code rule that maps each class label to its dummy code.
 #' @return A ggplot
 #' @importFrom ggplot2 ggplot geom_point aes_ sym
 #' @export
-plot.ARTMAP <- function(net, .data, classes = NULL, dummyCodeMap = NULL){
+plot.ARTMAP <- function(net, .data, x = NULL, y = NULL, classes = NULL, dummyCodeMap = NULL){
 
   if (!is.data.frame(.data)){
     .data <- tryCatch(
@@ -55,16 +66,23 @@ plot.ARTMAP <- function(net, .data, classes = NULL, dummyCodeMap = NULL){
     stop("The class labels are missing.")
   }
   # organize the data for plotting
-  if (isSimplified(net)){
+  if (is.null(dummyCodeMap)){
     .data <- cbind(.data, classes = classes)
   } else{
-    # convert the dummy codes to the class labels
-    if (is.null(dummyCodeMap)){
-      stop("You need to supply the dummy code mapping to convert the dummy codes into class labels.")
-    }
     .data <- cbind(.data, classes = decode(classes, dummyCodeMap))
   }
+  #if (isSimplified(net)){
+  #  .data <- cbind(.data, classes = classes)
+  #} else{
+    # convert the dummy codes to the class labels
+  #  if (is.null(dummyCodeMap)){
+  #    stop("You need to supply the dummy code mapping to convert the dummy codes into class labels.")
+  #  }
+  #  .data <- cbind(.data, classes = decode(classes, dummyCodeMap))
+  #}
   cols <- colnames(.data)
+  if (is.null(x)) x <- cols[1]
+  if (is.null(y)) y <- cols[2]
 
   # organize the weights for plotting
   module <- getModuleById(net, id = 0) # always plot ART a
@@ -76,7 +94,8 @@ plot.ARTMAP <- function(net, .data, classes = NULL, dummyCodeMap = NULL){
   }
 
   ggplot() + geom_point(data = .data, aes_(x = sym(cols[1]), y = sym(cols[2]), color = sym(cols[3]))) +
-    drawWeight(structure(module$w, class = append(class(module$w), getRule(net))), labels)
+    drawWeight(structure(module$w, class = append(class(module$w), getRule(net))), 
+               x = x, y = y, labels)
 }
 
 #' plot.TopoART
@@ -85,10 +104,14 @@ plot.ARTMAP <- function(net, .data, classes = NULL, dummyCodeMap = NULL){
 #' @param id The numeric id of the module to be plotted. The first module a (default) has an id = 0. Module b has an id = 1. The ids of the higher order modules are incremented.
 #' @param .data The data frame used for plotting the data. It must have 2 columns corresponding to the x values and y values, respectively.
 #'              If .data has more than 2 columns, only the first 2 columns will be used.
+#' @param x The variable to plot along the x axis. If it is not specified, then the first column of the data and the weight matrix
+#' will be used.
+#' @param y The variable to plot along the y axis. If it is not specified, then the second column of the data and the weight matrix
+#' will be used.
 #' @return A ggplot
 #' @importFrom ggplot2 ggplot geom_point aes_ sym
 #' @export
-plot.TopoART <- function(net, id = 0, .data){
+plot.TopoART <- function(net, id = 0, .data, x = NULL, y = NULL){
   if (is.matrix(.data)){
     .data <- tryCatch(
       as.data.frame(.data),
@@ -102,18 +125,25 @@ plot.TopoART <- function(net, id = 0, .data){
   }
   module <- getModuleById(net, id)
   categories <- getTopoClustersCategories(module)
-  cols <- colnames(.data[,1:2])
-  ggplot() + geom_point(data = .data, aes_(sym(cols[1]), sym(cols[2]))) +
-    drawWeight(structure(module$w, class = append(class(module$w), getRule(net))), categories)
+  
+  cols <- colnames(.data)
+  if (is.null(x)) x <- cols[1]
+  if (is.null(y)) y <- cols[2]
+  
+  ggplot() + geom_point(data = .data, aes_(sym(x), sym(y))) +
+    drawWeight(structure(module$w, class = append(class(module$w), getRule(net))), 
+               x = x, y = y, categories)
 }
 
 #' drawWeight
 #' @description Generic function for plotting weights
-#' @param x An ART object
+#' @param w The weight object
+#' @param x The x axis variable
+#' @param y The y axis variable
 #' @param ... Additional arguments to be passed
 #' @export
-drawWeight <- function(x, ...){
-  UseMethod("drawWeight", x)
+drawWeight <- function(w, x, y, ...){
+  UseMethod("drawWeight", w)
 }
 
 #' drawWeight.hypersphere
@@ -122,11 +152,11 @@ drawWeight <- function(x, ...){
 #' @param classLabels The vector of class labels associated with the weights in F2.
 #' @importFrom ggforce geom_circle
 #' @export
-drawWeight.hypersphere <- function(w, classLabels = NULL){
+drawWeight.hypersphere <- function(w, x, y, classLabels = NULL){
   if (!is.null(classLabels) && length(classLabels) != nrow(w))
     stop("The length of the class labels must be the same as the number of rows in the w matrix.")
 
-  w <- data.frame(x = w[,1], y = w[,2], R = w[,3])
+  w <- data.frame(x = w[,x], y = w[,y], R = w[,"R"])
   if (is.null(classLabels)){
     w <- cbind(w, label = rep("Cluster", nrow(w)))
     g <- geom_circle(data = w,
@@ -143,16 +173,21 @@ drawWeight.hypersphere <- function(w, classLabels = NULL){
 #' drawWeight.fuzzy
 #' @description Draw the 2D rectangular weights created by the fuzzy rule
 #' @param w The weight matrix
+#' @param x The x axis variable
+#' @param y The y axis variable
 #' @param classLabels The vector of class labels associated with all weights in F2
 #' @return A ggplot
 #' @importFrom ggplot2 geom_rect aes
 #' @export
-drawWeight.fuzzy <- function(w, classLabels = NULL){
+drawWeight.fuzzy <- function(w, x, y, classLabels = NULL){
   if (!is.null(classLabels) && length(classLabels) != nrow(w))
     stop("The length of the class labels must be the same as the number of rows in the w matrix.")
-
-  w <- data.frame(ux = w[,1], uy = w[,2],
-                  vx = 1-w[,3], vy = 1-w[,4])
+  
+  x_c <- paste0(x, "_c")
+  y_c <- paste0(y, "_c")
+  
+  w <- data.frame(ux = w[,x], uy = w[,y],
+                  vx = 1-w[,x_c], vy = 1-w[,y_c])
   if (is.null(classLabels)){
     w <- cbind(w, label = rep("Cluster", nrow(w)))
     g <- geom_rect(data = w,
